43c9798f41cbf3c793621bddabab3436ac6a18fe
homedesktop@2DGP <gunageroo@gmail.com>
2024-11-04 11:43:35 +0900

    진행을 위한 복붙

---
+++ control_boy.py
@@ -0,0 +1,69 @@
+from pico2d import *
+
+from grass import Grass
+from boy import Boy
+
+import game_world
+
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event)
+
+
+def reset_world():
+    global running
+#    global world
+    global boy
+
+    running = True
+#    world = []
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+#    world.append(grass)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+#    world.append(boy)
+
+
+
+def update_world():
+    game_world.update()
+#    for o in world:
+#        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    game_world.render()
+#    for o in world:
+#        o.draw()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
+
+
---
+++ game_world.py
@@ -0,0 +1,29 @@
+# wordl [0]:백그라운드 객체들
+# world[1] : foreground 객체들
+world = [[],[]]     # <- 월드리스트를 리스트 of 리스트로 변경
+
+
+def add_object(o, depth):
+    world[depth].append(o)
+
+
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
+
+
+def remove_object(o):       #다만 여기서 없는 o존재를 remove하면 프로그램이 죽으니까
+    #조건문을 추가.
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
+    print(f'CRITICAL : 존재하지 않는 객체{o}를 지우려고 시도함')
